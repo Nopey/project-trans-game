9cf03147213a02d422994f641509fc07c5aee9ab
Maggie L <golgothasTerror101@gmail.com>
2026-06-08 16:40:15 -0600

    Dialog Editor: Avoid deprecated method

--- addons/dialog_editor/dialog_editor.gd
+++ addons/dialog_editor/dialog_editor.gd
@@ -7,9 +7,9 @@ func _enter_tree() -> void:
 	dialog_editor_menu = preload("res://addons/dialog_editor/dialog_ui.tscn").instantiate()
 	add_custom_type("DialogReader", "Node", preload("res://addons/dialog_editor/dialog_reader.gd"), preload("res://addons/dialog_editor/DialogEditorIcon.svg"))
 	#add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_UL, my_custom_menu)
-	add_control_to_bottom_panel(dialog_editor_menu, "Dialog Editor")
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_BOTTOM, dialog_editor_menu)
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
-	remove_control_from_bottom_panel(dialog_editor_menu)
+	remove_control_from_docks(dialog_editor_menu)
 	remove_custom_type("DialogReader")
