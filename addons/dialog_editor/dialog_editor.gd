41ea5c8be993bfd9267908dc3a10f4800ffc5854
Maggie L <golgothasTerror101@gmail.com>
2026-06-08 16:09:52 -0600

    Dialog Editor: Minor fixes

--- addons/dialog_editor/dialog_editor.gd
+++ addons/dialog_editor/dialog_editor.gd
@@ -7,7 +7,7 @@ func _enter_tree() -> void:
 	dialog_editor_menu = preload("res://addons/dialog_editor/dialog_ui.tscn").instantiate()
 	add_custom_type("DialogReader", "Node", preload("res://addons/dialog_editor/dialog_reader.gd"), preload("res://addons/dialog_editor/DialogEditorIcon.svg"))
 	#add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_UL, my_custom_menu)
-	add_control_to_bottom_panel(dialog_editor_menu, "dialog Editer")
+	add_control_to_bottom_panel(dialog_editor_menu, "Dialog Editor")
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
