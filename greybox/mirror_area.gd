0da26488cd9871c6ff306dca6aac1e316546f3a5
Maggie L <golgothasTerror101@gmail.com>
2026-06-09 20:59:29 -0600

    Greybox: Revert to isometric RPG controls

--- greybox/mirror_area.gd
+++ greybox/mirror_area.gd
@@ -5,7 +5,6 @@ extends Area3D
 func _on_body_entered(body: Node3D) -> void:
 	if body is FreePlayer:
 		sprite.show()
-		print("show")
 
 func _on_body_exited(body: Node3D) -> void:
 	if body is FreePlayer:
